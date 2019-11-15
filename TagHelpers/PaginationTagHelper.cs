fa065d0b8caa3b0de8bc28cc6c1dbbb1f7958eef
Andrey Yanchuk <and.yanchuk@gmail.com>
2019-11-15 14:55:49 +0200

    Pagination stilization added

--- TagHelpers/PaginationTagHelper.cs
+++ TagHelpers/PaginationTagHelper.cs
@@ -17,21 +17,30 @@ namespace Blog.TagHelpers
         {
             urlHelperFactory = helperFactory;
         }
-        
+
         [ViewContext]
         [HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
         public PageModel PageModel { get; set; }
+        public string PageClass { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClassSelected { get; set; }
+        public string PageClassNormal { get; set; }
         public string PageAction { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext); 
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
             TagBuilder result = new TagBuilder("div");
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.TagRenderMode = TagRenderMode.Normal;
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);   
+                }
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
